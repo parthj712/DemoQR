8031b0b460c07ffefb707b7f8786fa7b477f5821
parth <parth.j@coditude.com>
2025-09-19 10:00:14 +0530

    add lang context

---
+++ src/Context/LanguageContext.js
@@ -0,0 +1,25 @@
+'use client';
+
+import { createContext, useContext, useState } from 'react';
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('mr'); // default to Marathi
+
+  const toggleLanguage = () => {
+    setLanguage(prev => (prev === 'en' ? 'mr' : 'en'));
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, toggleLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (!context) throw new Error('useLanguage must be used within LanguageProvider');
+  return context;
+};
