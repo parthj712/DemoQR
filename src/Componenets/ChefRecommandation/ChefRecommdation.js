4364d718fbacc09b7c60c182e3bd1ac47022d30d
parth <parth.j@coditude.com>
2025-09-21 23:58:08 +0530

    fix scroll chef section

--- src/Componenets/ChefRecommandation/ChefRecommdation.js
+++ src/Componenets/ChefRecommandation/ChefRecommdation.js
@@ -30,28 +30,35 @@ const ChefRecommdation = ({ setSelectedDish }) => {
                 <HeadingEMenu />
             </Box>
 
-            {recommendations.length > 0 && (
-                <Box mb={4} px={2}>
-                    <Typography variant="h6" py={1.5}>
-                        {chef}
-                    </Typography>
+            <Box
+                flex={1}
+                overflow="auto"   // ✅ allow scrolling
+                px={2}
+                          // space for BottomNav if fixed
+            >
+                {recommendations.length > 0 && (
+                    <Box mb={10} p={2}>
+                        <Typography variant="h6" py={1.5}>
+                            {chef}
+                        </Typography>
 
-                    {recommendations.map((dish) => (
-                        <Card
-                            key={`${dish.sectionKey}-${dish.itemId}`} // ✅ unique key across sections
-                            sx={{ mb: 2, borderRadius: 2, cursor: "pointer" }}
-                            onClick={() => setSelectedDish(dish)} // open popup
-                        >
-                            <CardContent>
-                                <Typography variant="h6">{language === 'mr' ? (dish.title_mr || "") : (dish.title_en || "")}</Typography>
-                                <Typography color="text.secondary">
-                                    {dish.price_en}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    ))}
-                </Box>
-            )}
+                        {recommendations.map((dish) => (
+                            <Card
+                                key={`${dish.sectionKey}-${dish.itemId}`} // ✅ unique key across sections
+                                sx={{ mb: 2, borderRadius: 2, cursor: "pointer" }}
+                                onClick={() => setSelectedDish(dish)} // open popup
+                            >
+                                <CardContent>
+                                    <Typography variant="h6">{language === 'mr' ? (dish.title_mr || "") : (dish.title_en || "")}</Typography>
+                                    <Typography color="text.secondary">
+                                        {dish.price_en}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </Box>
+                )}
+            </Box>
         </>
     );
 };
